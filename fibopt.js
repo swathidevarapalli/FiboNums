74a9b0a70d051b353ad1e5b6f1f61045114b2aae
swathidevaraplli <swathi.d118@gmail.com>
2021-10-19 16:26:55 +0530

    First 100 Fibonacci Numbers

---
+++ fibopt.js
@@ -0,0 +1,11 @@
+var fib = function(n) {
+    if (n === 1) {
+      return [0, 1];
+    } else {
+      var arr = fib(n - 1);
+      arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+      return arr;
+    }
+  };
+  
+  console.log(fib(100));
